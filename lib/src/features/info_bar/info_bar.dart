d3818cc4bb50c2a1a89387803602da2e8dd70eb8
FNIT <fnit@FNITs-Mac-Studio.local>
2023-08-10 15:03:37 +0400

    personal information

--- lib/src/features/info_bar/info_bar.dart
+++ lib/src/features/info_bar/info_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/src/constants/app_colors.dart';
+import 'package:portfolio/src/features/info_bar/components/address_card.dart';
 import 'package:portfolio/src/features/info_bar/components/photo_title_card.dart';
 
 class InfoBar extends StatelessWidget {
@@ -6,10 +8,15 @@ class InfoBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: <Widget>[
-        PhotoAndTitleCard(),
-      ],
+    return Container(
+      color: AppColors.navBgcolor,
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          PhotoAndTitleCard(),
+          AddressCard(),
+        ],
+      ),
     );
   }
 }
