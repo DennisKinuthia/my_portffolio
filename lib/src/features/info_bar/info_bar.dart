cc2984d89d9d0410ade092bfb12c1975a8ef0744
FNIT <fnit@FNITs-Mac-Studio.local>
2023-08-10 13:44:15 +0400

    extract photoAndTitleCard widget for composition

--- lib/src/features/info_bar/info_bar.dart
+++ lib/src/features/info_bar/info_bar.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:portfolio/src/constants/app_colors.dart';
-import 'package:portfolio/src/constants/app_sizes.dart';
-import 'package:portfolio/src/features/info_bar/components/pulsating_dot.dart';
+import 'package:portfolio/src/features/info_bar/components/photo_title_card.dart';
 
 class InfoBar extends StatelessWidget {
   const InfoBar({super.key});
@@ -11,72 +8,8 @@ class InfoBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: <Widget>[
-        BusinessCard(),
+        PhotoAndTitleCard(),
       ],
-    );
-  }
-}
-
-class BusinessCard extends StatelessWidget {
-  const BusinessCard({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.bsCardColor,
-      padding: const EdgeInsets.symmetric(
-        horizontal: Sizes.p64,
-        vertical: Sizes.p32,
-      ),
-      child: Column(
-        children: <Widget>[
-          const Stack(
-            children: <Widget>[
-              Opacity(
-                opacity: .8,
-                child: CircleAvatar(
-                  radius: Sizes.p48,
-                  backgroundImage:
-                      AssetImage('assets/images/dennis_headshot.jpg'),
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: Tooltip(
-                    message: 'I am available for freelance hire',
-                    child: PulsatingDot()),
-              ),
-            ],
-          ),
-          gapH16,
-          Text(
-            'Dennis Kinuthia',
-            style: GoogleFonts.roboto(
-              fontSize: Sizes.p16,
-              fontWeight: FontWeight.w700,
-              color: AppColors.headlineColor,
-            ),
-          ),
-          gapH8,
-          Text(
-            'Full Stack Developer,',
-            style: GoogleFonts.roboto(
-              fontSize: Sizes.p12,
-              color: AppColors.textColor,
-            ),
-          ),
-          Text(
-            'Flutter & Firebase/NodeJs',
-            style: GoogleFonts.roboto(
-              fontSize: Sizes.p12,
-              color: AppColors.textColor,
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
